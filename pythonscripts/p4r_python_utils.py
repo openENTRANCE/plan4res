9151abd4ca03f39ebab22592926b404c8982a46b
sandrinecharousset <62063474+sandrinecharousset@users.noreply.github.com>
2024-10-01 09:39:03 +0200

    update from plan4res-scripts

--- pythonscripts/p4r_python_utils.py
+++ pythonscripts/p4r_python_utils.py
@@ -24,7 +24,30 @@ def read_input_csv(cfg, file_name_key, **kwargs):
 	if not os.path.isfile(file):
 		logger.error('File '+file+' does not exist. Use key inputpath in configuration file to specify input directory.')
 		log_and_exit(2, cfg['path'])
+	if 'csv_delim' in cfg.keys():
+		kwargs.update({'sep' : cfg['csv_delim']})
+	logger.info('Read file '+file)
 	return pd.read_csv(file, **kwargs)
+
+def save_input_csv(cfg, file_name_key,data, index=False, **kwargs):
+	file = os.path.join(cfg['inputpath'], cfg['csvfiles'][file_name_key])
+	indexSave=0
+	while os.path.isfile(os.path.join(cfg['inputpath'], cfg['csvfiles'][file_name_key]+'.save_'+str(indexSave)+'.csv')):
+		indexSave=indexSave+1
+	fileSave = os.path.join(cfg['inputpath'], cfg['csvfiles'][file_name_key]+'.save_'+str(indexSave)+'.csv')
+	if 'csv_delim' in cfg.keys():
+		kwargs.update({'sep' : cfg['csv_delim']})
+	#data.to_csv(fileSave,**kwargs)
+	data.to_csv(fileSave,index=index, **kwargs)
+	
+def write_input_csv(cfg, file_name_key,data, index=False, **kwargs):
+	file = os.path.join(cfg['inputpath'], cfg['csvfiles'][file_name_key])
+	indexSave=0
+	if 'csv_delim' in cfg.keys():
+		kwargs.update({'sep' : cfg['csv_delim']})
+	#data.to_csv(file,**kwargs)
+	data.to_csv(file,index=index, **kwargs)
+
 
 def check_and_read_csv(cfg, file, **kwargs):
 	if not os.path.isfile(file):
